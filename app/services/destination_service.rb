6109f4210ba5e7722a6785b08de11733e9c1e066
Mike Schutte <tmikeschutte@gmail.com>
2017-02-19 13:10:41 -0700

    Add wunderground key

--- app/services/destination_service.rb
+++ app/services/destination_service.rb
@@ -1,6 +1,29 @@
 class DestinationService
+  def initialize(destination_zip)
+    @zip = destination_zip
+  end
 
-  def forecast_10_day(destination_zip)
+  def self.forecast_10_day(destination_zip)
+    new(destination_zip).days
+  end
 
+  def connection
+    @conn ||= Faraday.new("http://api.wunderground.com/api/")
+  end
+
+  def response
+    connection.get(default_api_path).body
+  end
+
+  def default_api_path
+    "#{ENV["WUNDERGROUND_KEY"]}/forecast10day/q/#{@zip}"
+  end
+
+  def json_days
+    JSON.parse(response, symbolize_names: true)
+  end
+
+  def days
+    json_days.map { |day| Day.new(day) }
   end
 end
