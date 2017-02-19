284b72b4b879f201febfe2853d4dc262b9491fcc
Mike Schutte <tmikeschutte@gmail.com>
2017-02-19 12:57:54 -0700

    Setup VCR

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -26,6 +26,11 @@ require 'rspec/rails'
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
--- spec/services/destination_service_spec.rb
+++ spec/services/destination_service_spec.rb
@@ -4,11 +4,13 @@ RSpec.describe DestinationService do
   context "methods" do
     describe ".forecast_10_day" do
       it "returns an array of 10 day objects" do
-        result = DestinationService.forecast_10_day(80218)
+        VCR.use_cassette("forecast-10-day-80218") do
+          result = DestinationService.forecast_10_day(80218)
 
-        expect(result).to be_a(Array)
-        expect(result.all? { |day| day.class == Day }).to be true
-        expect(result.count).to eq 10
+          expect(result).to be_a(Array)
+          expect(result.all? { |day| day.class == Day }).to be true
+          expect(result.count).to eq 10
+        end
       end
     end
   end
