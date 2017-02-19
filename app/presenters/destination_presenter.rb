8fd0ab1743feb9a58ad7c00fa2662b16a9133ff8
Mike Schutte <tmikeschutte@gmail.com>
2017-02-19 12:21:09 -0700

    Develop forecast model

--- app/presenters/destination_presenter.rb
+++ app/presenters/destination_presenter.rb
@@ -1,3 +1,21 @@
 class DestinationPresenter
+  attr_reader :destination
 
+  delegate :name,        to: :destination, prefix: true
+  delegate :zip,         to: :destination, prefix: true
+  delegate :description, to: :destination, prefix: true
+  delegate :image_url,   to: :destination, prefix: true
+
+  def initialize(destination)
+    @destination = destination
+  end
+
+  def self.present(destination)
+    new(destination)
+  end
+
+  def forecast_days
+    # DestinationService.get_forecast_by_zip(destination_zip)
+    Forecast.for_zip(destination_zip)
+  end
 end
--- spec/presenters/destination_presenter_spec.rb
+++ spec/presenters/destination_presenter_spec.rb
@@ -2,15 +2,24 @@ require "rails_helper"
 
 RSpec.describe DestinationPresenter, type: :model do
   context "methods" do
+    let(:destination) { Destination.create(name:"Miami", zip:"33018", description: "Warm & Sunny", image_url: "google.com") }
+    let(:presenter)   { DestinationPresenter.present(destination) }
+
     describe ".present" do
       it "returns a presenter instance with destination attributes" do
-        destination = Destination.create(
-          name:"Miami",
-          zip:"33018",
-          description: "Warm & Sunny",
-          image_url: "google.com"
-        )
-        presenter = DestinationPresenter.present()
+
+        expect(presenter).to respond_to(:destination_name)
+        expect(presenter).to respond_to(:destination_zip)
+        expect(presenter).to respond_to(:destination_description)
+        expect(presenter).to respond_to(:destination_image_url)
+        expect(presenter).to respond_to(:forecast_days)
+      end
+    end
+
+    describe "#forecast_days" do
+      it "returns an array of 10 forecasts" do
+        # expect(thing).to be_a(Destination::Array)
+
       end
     end
   end
